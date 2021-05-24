e482b1b29a54d2f287279ca947b84c72814cafb8
yuto-dev <directx178@gmail.com>
2021-05-25 03:53:50 +0800

    Foundations for registration, display, and sort features finished.

--- src/main/java/com/mycompany/assignment/LoginFrame.java
+++ src/main/java/com/mycompany/assignment/LoginFrame.java
@@ -1,9 +1,11 @@
 package com.mycompany.assignment;
 
+import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JTextField;
 
 public class LoginFrame extends JFrame implements ActionListener {
     
@@ -11,12 +13,13 @@ public class LoginFrame extends JFrame implements ActionListener {
     
     LoginFrame(){
         
-        loginButton = new JButton();
+        JTextField logintext = new JTextField();    
+        loginButton = new JButton("Login");
         loginButton.setBounds(200, 100, 100, 50);
         loginButton.addActionListener(this);
         
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setLayout(null);
+        this.setLayout(new FlowLayout());
         this.setSize(360,360);
         this.setTitle("Login");
         this.setVisible(true);
